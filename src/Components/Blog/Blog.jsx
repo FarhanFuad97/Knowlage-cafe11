11a2986ad017f2079456cfcfb86705be98eac50c
FarhanFuad97 <farhanfuad783@gmail.com>
2023-09-15 16:12:50 +0600

    addad a lot of thinks

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,28 @@
 
 import PropTypes from 'prop-types'; 
 const Blog = ( {blog} ) => {
-    console.log(blog);
+    const {title,cover,author,author_img,posted_date,reading_time,hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-96' src={cover}/>
+            <div className='flex justify-between'>
+                <div className='flex p-2'>
+            <img className='w-14' src={author_img} alt="" />
+            <div className='ml-4'>
+                <h3 className='text-xl'>{author}</h3>
+                <p>{posted_date}</p>
+            </div>
+                </div>
+                <div>
+            <span>{reading_time} min red</span>
+                </div>
+            </div>
+            <h2 className='text-2xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash,idx) => <span key={idx}> <a href=''>{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@
 import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <header className='flex justify-between p-4 items-center border-b'>
+        <header className='flex justify-between p-4 items-center border-b max-w-7xl mx-auto'>
         <h1 className='text-4xl font-bold '>Knowlage Cafe</h1>
           <img src={profile} alt="" />
         </header>
